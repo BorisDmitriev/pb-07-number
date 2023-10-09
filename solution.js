bc612e105a24934984806e7be4b8936c5e3bbb67
BorisDmitriev <113904873+BorisDmitriev@users.noreply.github.com>
2023-10-09 20:56:14 +0200

    Initial commit

---
+++ solution.js
@@ -0,0 +1,13 @@
+// 1
+const introSentence = "hello, my name is Fran and I am";
+const age = 25;
+// + concatenates two strings
+console.log(introSentence + " " + age);
+
+//2
+const integer = "1005";
+console.log(parseInt(integer));
+
+const float = "10.05";
+console.log(parseFloat(float));
+
